dcf6387a58bd3b01ff065536c55816131a4854e6
achraf-fouad <2005022600309@ofppt-edu.ma>
2026-03-12 09:08:29 +0000

    fix config  error

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['images.unsplash.com', 'tw8i3g-dr.myshopify.com', 'your-project-id.supabase.co'],
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: '**.supabase.co',
-      },
-    ],
+        protocol: "https",
+        hostname: "**"
+      }
+    ]
   },
   reactCompiler: true,
 };
